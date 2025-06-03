1fb5d03f7cbfad45f2151f398c699b1ade292c03
marcolion23 <marcoafslion23@gmail.com>
2025-06-03 17:26:17 -0300

    atualizando1

--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -9,11 +9,20 @@ import { Router } from '@angular/router';
 })
 
 export class ProductCrudComponent implements OnInit {
+  mostrarTabela = false;
+
+  colunasExibidas: string[] = ['id', 'nome', 'custo', 'venda', 'acoes'];
 
   //construtor para configurar botao para tela de produto
   constructor(private router: Router) { }
-
+  produtos = [
+    { id: 1, nome: 'Mouse Gamer RGB', custo: 50, venda: 99 },
+    { id: 2, nome: 'Teclado Mecânico', custo: 120, venda: 199 }
+  ];
   ngOnInit(): void {
+  }
+  novoProduto() {
+    alert('Abrir formulário de novo produto (você pode fazer um dialog)');
   }
 
   //criando interação com botoes
