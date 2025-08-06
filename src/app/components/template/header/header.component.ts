4fcf595f413ab5d5da7aed280bfa2219eabf9733
marcolion23 <marcoafslion23@gmail.com>
2025-08-06 17:07:11 -0300

    atualizando projeto

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -10,11 +10,32 @@ export class HeaderComponent {
   constructor(private sidenavService: SidenavService) {}
 
   menuOpen = true;
+quickAccessOpen = false;
+autoCloseTimeout: any;
+
+  // ✅ NOVO: painel de acesso rápido
 
   onToggleMenu() {
     this.menuOpen = !this.menuOpen;
-    this.sidenavService.toggle(); // <-- aciona a abertura/fechamento do menu
-  }
-  
+    this.sidenavService.toggle(); // <-- aciona a abertura/fechamento do menu esquerdo
   }
 
+toggleQuickAccess() {
+  this.quickAccessOpen = !this.quickAccessOpen;
+
+  if (this.quickAccessOpen) {
+    // inicia temporizador para fechar automaticamente
+    clearTimeout(this.autoCloseTimeout);
+    this.autoCloseTimeout = setTimeout(() => {
+      this.quickAccessOpen = false;
+    }, 5000); // fecha após 5s
+  } else {
+    clearTimeout(this.autoCloseTimeout);
+  }
+}
+
+closeQuickAccess() {
+  this.quickAccessOpen = false;
+  clearTimeout(this.autoCloseTimeout);
+}
+}
